0f5b8f7095e689bd2c4e6cc0828846e584ac6176
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-06-15 21:20:56 -0300

    feat: implement repository interface shared

---
+++ src/domain/@shared/repository/repositoryInterface.ts
@@ -0,0 +1,7 @@
+export interface RepositoryInterface<T> {
+  create(entity: T): Promise<T>;
+  update(entity: T): Promise<T>;
+  delete(id: string): Promise<void>;
+  findById(id: string): Promise<T>;
+  findAll(): Promise<T[]>;
+}
