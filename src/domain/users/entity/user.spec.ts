e52929f197347a3bc8d81993bbd080b8da959eea
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-06-15 21:07:07 -0300

    feat: implement entity user and validation test

---
+++ src/domain/users/entity/user.spec.ts
@@ -0,0 +1,9 @@
+import { User } from "./user";
+
+describe("Users unit test", () => {
+  it("should throw error when name is empty", () => {
+    expect(
+      () => new User("1", "", "email@email.com", "password123")
+    ).toThrowError("User name must be required from create user");
+  });
+});
---
+++ src/domain/users/entity/user.ts
@@ -0,0 +1,21 @@
+export class User {
+  private _id: string;
+  private _name: string;
+  private _email: string;
+  private _password: string;
+
+  constructor(id: string, name: string, email: string, password: string) {
+    this._id = id;
+    this._name = name;
+    this._email = email;
+    this._password = password;
+
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("User name must be required from create user");
+    }
+  }
+}
